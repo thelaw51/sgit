09863d145e3c739e029900217aed84ab47997789
Joe <joegillard3@gmail.com>
2025-06-17 22:36:34 +0100

    started some code mostly just remebering rust stuff and learning new stuff got a basic start though

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+/// A friendly CLI application
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Init,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Init) => {
+            InitNewRepo();
+        }
+        None => {
+            println!("No command specified. Use --help for more information.");
+        }
+    }
+}
+
+fn InitNewRepo() {}
