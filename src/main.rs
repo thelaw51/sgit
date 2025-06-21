1e27e3d54e9f1ac1834f268b1ca1517a4d18b77f
Joe <joegillard3@gmail.com>
2025-06-21 11:59:07 +0100

    putting on pause not sure interesting but dont feel like the infomation is sticking right now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::path::Path;
+
 use clap::{Parser, Subcommand};
 
 /// A friendly CLI application
@@ -18,7 +21,7 @@ fn main() {
 
     match &cli.command {
         Some(Commands::Init) => {
-            InitNewRepo();
+            init_new_repo();
         }
         None => {
             println!("No command specified. Use --help for more information.");
@@ -26,4 +29,23 @@ fn main() {
     }
 }
 
-fn InitNewRepo() {}
+fn create_dir(dir_path: String) {
+    let create_result = fs::create_dir(&dir_path);
+
+    match create_result {
+        Ok(_) => {
+            // Directory created successfully or already existed
+            if Path::new(&dir_path).is_dir() {
+                println!("'{}' Generated...", dir_path);
+            }
+        }
+        Err(e) => {
+            // Handle the error, e.g., print it
+            eprintln!("Error creating directory '{}': {}", dir_path, e);
+        }
+    }
+}
+
+fn init_new_repo() {
+    create_dir("./.sgit".to_string());
+}
